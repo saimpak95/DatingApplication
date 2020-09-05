99aa3a12a02c3ee706dafb99efbb4f44110cac74
Saim Siddiqui <saimpak95@yahoo.com>
2020-09-05 19:32:30 +0500

    Routing

---
+++ DatingApp-SPA/src/app/routes.ts
@@ -0,0 +1,23 @@
+import {Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
+
+export const appRoute: Routes = [
+    {path : '', component: HomeComponent},
+    {path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+
+        {path : 'members', component: MemberListComponent},
+        {path : 'lists', component: ListsComponent},
+        {path : 'message', component: MessagesComponent},
+
+    ]
+
+    },
+    {path : '**', redirectTo: '', pathMatch: 'full'},
+];
